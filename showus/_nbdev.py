f2a3201ace6662efb136ff45da7f674ee93a3d57
Jack Yu <jackchungchiehyu@gmail.com>
2021-05-17 08:41:05 +0800

    Import showus.ipynb from Kaggle

--- showus/_nbdev.py
+++ showus/_nbdev.py
@@ -2,9 +2,28 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"Path.ls": "showus.ipynb",
+         "load_train_meta": "showus.ipynb",
+         "load_papers": "showus.ipynb",
+         "load_sample_text": "showus.ipynb",
+         "clean_training_text": "showus.ipynb",
+         "shorten_sentences": "showus.ipynb",
+         "find_sublist": "showus.ipynb",
+         "tag_sentence": "showus.ipynb",
+         "get_ner_data": "showus.ipynb",
+         "write_ner_json": "showus.ipynb",
+         "kaggle_run_ner": "showus.ipynb",
+         "create_knowledge_bank": "showus.ipynb",
+         "literal_match": "showus.ipynb",
+         "get_ner_inference_data": "showus.ipynb",
+         "kaggle_run_ner_predict": "showus.ipynb",
+         "run_inference": "showus.ipynb",
+         "get_bert_dataset_labels": "showus.ipynb",
+         "jaccard_similarity": "showus.ipynb",
+         "filter_bert_labels": "showus.ipynb",
+         "combine_matching_and_bert": "showus.ipynb"}
 
-modules = []
+modules = ["showus.py"]
 
 doc_url = "https://qAp.github.io/showus/"
 
